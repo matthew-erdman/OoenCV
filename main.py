86c2da2726c16fa5d1fae2e5e39eaca7f30fc427
matthew-erdman <matthew.erdman.23@bishops.com>
2022-10-05 22:34:55 -0700

    Card detection

--- main.py
+++ main.py
@@ -3,21 +3,39 @@ import argparse
 import cv2
 import mahotas
 
+def readCards(frame, cnts, padding):
+    cards = []
+    for (i, c) in enumerate(cnts):
+        # find bounding box of each card
+        (x, y, w, h) = cv2.boundingRect(c)
+        # locate card with padding
+        card = frame[y - padding:y + h + padding, x - padding:x + w + padding]
+        cards.append(card)
+
+    return cards
+
+
 def findContours(frame):
     # find edges in frame
     edges = cv2.Canny(frame, 30, 150)
     # find outermost contours in frame, edges of each card
     (cnts, _) = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    return cnts
+    validBorders = []
+    for c in cnts:
+        # find bounding box of each card
+        (x, y, w, h) = cv2.boundingRect(c)
+        # ensure contour is large enough to be a card border
+        if w > 200 and h > 300:
+            validBorders.append(c)
+    return validBorders
+
 
 def preprocess(frame):
     # grayscale frame
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # blur frame
-    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
-
+    blurred = cv2.GaussianBlur(gray, (15, 15), 0)
     return blurred
 
 
@@ -35,10 +53,15 @@ def main():
         # grayscale and blur frame
         processed = preprocess(frame)
 
-        # find card-like contours in the frame
-        cnts = findContours(processed)
+        # find card border-like contours in the frame
+        borders = findContours(processed)
+        cardVals = readCards(processed, borders, 20)
+        for i, card in enumerate(cardVals):
+            cv2.imshow("card " + str(i), card)
         cards = frame.copy()
-        cv2.drawContours(cards, cnts, -1, (0, 255, 0), 2)
+        cv2.drawContours(cards, borders, -1, (0, 255, 0), 2)
+
+        image = cv2.putText(cards, str(len(borders)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
         cv2.imshow('Cards', cards)
         if cv2.waitKey(1) != -1:
