72ac0ee72ae2fb1c8b0f456e22e21b35ad0f6de2
matthew-erdman <matthew.erdman.23@bishops.com>
2022-10-20 00:30:13 -0700

    Rank detection and parsing

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
 import numpy as np
-import argparse
 import cv2
-import mahotas
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'/usr/local/Cellar/tesseract/5.0.1/bin/tesseract'
 
-def readCards(frame, cnts, padding):
+
+def straightenCard(c, frame):
+    # find rotated bounding box of card
+    bounding = cv2.minAreaRect(c)
+    h = int(bounding[1][0])
+    w = int(bounding[1][1])
+    box = np.float32(cv2.boxPoints(bounding))
+
+    # straighten card
+    dst = np.array([[0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1]], dtype="float32")
+    straightenedMatrix = cv2.getPerspectiveTransform(box, dst)
+    straightened = cv2.warpPerspective(frame, straightenedMatrix, (w, h))
+    w = straightened.shape[0]
+    h = straightened.shape[1]
+
+    # fix cards straightened with an angle less than 45 degrees
+    if bounding[2] < 45.0:
+        straightened = cv2.rotate(straightened, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+    return straightened
+
+
+def readCards(frame, cnts):
     cards = []
     for (i, c) in enumerate(cnts):
-        # find bounding box of each card
-        (x, y, w, h) = cv2.boundingRect(c)
-        # locate card with padding
-        card = frame[y - padding:y + h + padding, x - padding:x + w + padding]
-        cards.append(card)
+        # straighten card
+        straightened = straightenCard(c, frame)
+        h = straightened.shape[0]
+        w = straightened.shape[1]
+
+        # focus on top left rank
+        roi = straightened[0:int(h*0.3), 0:int(w*0.2)]
+
+        # attempt single character ocr with whitelist
+        char = pytesseract.image_to_string(roi, config="--psm 10 -c tessedit_char_whitelist=0123456789AJQK").strip()
+
+        # no valid character found in top left, fallback to top right
+        if not char:
+            roi = straightened[0:int(h * 0.3), int(w * 0.8):w]
+            char = pytesseract.image_to_string(roi, config="--psm 10 -c tessedit_char_whitelist=0123456789AJQK").strip()
+
+        # parse ocr results
+        if "A" in char:
+            char = "Ace"
+        elif "J" in char:
+            char = "Jack"
+        elif "Q" in char:
+            char = "Queen"
+        elif "K" in char or "k" in char:
+            char = "King"
+        elif char == "0":
+            char = "10"
+        cards.append(char)
+
+        cv2.imshow("straightened " + str(i), straightened)
+        cv2.imshow("roi " + str(i), roi)
 
     return cards
 
@@ -43,28 +91,36 @@ def main():
     # initialize video capture device
     video = cv2.VideoCapture(0)
     # video loop
-    while (True):
+    while True:
         ret, frame = video.read()
         # no frame received from camera
         if not ret:
             print("No frame received from camera - exiting...")
             break
 
-        # grayscale and blur frame
-        processed = preprocess(frame)
+        keypress = cv2.waitKey(1)
 
-        # find card border-like contours in the frame
-        borders = findContours(processed)
-        cardVals = readCards(processed, borders, 20)
-        for i, card in enumerate(cardVals):
-            cv2.imshow("card " + str(i), card)
-        cards = frame.copy()
-        cv2.drawContours(cards, borders, -1, (0, 255, 0), 2)
+        cv2.imshow('Video', frame)
 
-        image = cv2.putText(cards, str(len(borders)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        # analyze selected frame
+        if keypress == ord(" "):
+            cv2.destroyAllWindows()
+            # grayscale and blur frame
+            processed = preprocess(frame)
+            # find card border-like contours in the frame
+            borders = findContours(processed)
+            # detect card values
+            cardVals = readCards(frame, borders)
 
-        cv2.imshow('Cards', cards)
-        if cv2.waitKey(1) != -1:
+            # display results
+            cards = frame.copy()
+            cv2.drawContours(cards, borders, -1, (0, 255, 0), 2)
+            image = cv2.putText(cards, str(cardVals), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.imshow('Cards', cards)
+            if cv2.waitKey(0) in [ord("q"), ord("Q")]:
+                break
+
+        if keypress in [ord("q"), ord("Q")]:
             break
 
     video.release()
